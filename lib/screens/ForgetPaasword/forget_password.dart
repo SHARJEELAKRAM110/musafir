378248be6a83116d09177088cb9b59fdc8432e3e
SHARJEEL AKRAM <dawaits683@gmail.com>
2023-09-13 11:42:29 +0500

    Sharjeel

--- lib/screens/ForgetPaasword/forget_password.dart
+++ lib/screens/ForgetPaasword/forget_password.dart
@@ -1,6 +1,9 @@
+import 'package:country_list_pick/country_list_pick.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:musafir/Widgets/CustomWidgets/custom_text.dart';
+
+import '../../Widgets/Round Button.dart';
 
 class ForgetPassword extends StatefulWidget {
   const ForgetPassword({super.key});
@@ -40,7 +43,7 @@ SizedBox(height: 118.h,),
       SizedBox(
         width: 270,
         child: CustomText(text:
-          'Enter your registered email or mobile number below',
+          'Enter your mobile number below.',
             color: Color(0xFF9CA3AF),
             fontSize: 16,
             fontFamily: 'Inter',
@@ -49,12 +52,94 @@ SizedBox(height: 118.h,),
         ),
       SizedBox(height: 33.h,),
       CustomText(text:
-        'Email or Mobile number',
+        'Mobile number',
           color: Color(0xFF9CA3AF),
           fontSize: 14,
           fontFamily: 'Inter',
           fontWeight: FontWeight.w600,
         ),
+      SizedBox(height: 5.h,),
+
+      SizedBox(
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10.0),
+
+              ),
+              height: 45.h,
+              width: 100.w,
+              child: CountryListPick(
+
+                theme: CountryTheme(
+                  isShowFlag: true,
+                  isShowTitle: false,
+                  isShowCode: true,
+                  isDownIcon: false,
+                  showEnglishName: false,
+                ),
+                // Set default value
+                initialSelection: '+92',
+                // or
+                // initialSelection: 'US'
+                onChanged: (CountryCode? code) {
+                },
+              ),
+            ),
+            SizedBox(
+              height: 50.h,
+              width: 230.w,
+              child: TextField(
+
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(top: 5.h),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Color(0xffD9D9D9))),
+                  enabledBorder:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                  hintText: 'Enter Phone Number',
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+      SizedBox(height: 20.h,),
+      Text.rich(
+        TextSpan(
+          children: [
+            TextSpan(
+              text: 'Remember the password?',
+              style: TextStyle(
+                color: Color(0xFF9CA3AF),
+                fontSize: 14,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+                height: 0,
+              ),
+            ),
+            TextSpan(
+              text: ' Sign in',
+              style: TextStyle(
+                color: Color(0xFF4AC3B4),
+                fontSize: 14,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+                height: 0,
+              ),
+            ),
+          ],
+        ),
+      ),
+  SizedBox(height: 200.h,),
+  RoundButton(title: "Login", onpress: (){},),
 
 
     ],
--- lib/screens/SiginScreen/create_account.dart
+++ lib/screens/SiginScreen/create_account.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../Constants/Colors.dart';
+import '../../Widgets/CustomWidgets/custom_text.dart';
+import '../../Widgets/Round Button.dart';
+import '../../Widgets/customtext_formfield.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
@@ -8,8 +14,178 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  TextEditingController emailController=TextEditingController();
+  TextEditingController passwordController=TextEditingController();
+  TextEditingController confirmpasswordController=TextEditingController();
+  bool isChecked=false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 40.h,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(width: 20.w,),
+                Image.asset("assets/images/logo-google.png",height: 20.h,width: 20.w,),
+                SizedBox(width: 40.w,),
+
+                CustomText(text:
+                'Continue with Google',
+                  textAlign: TextAlign.center,
+                  color: Color(0xFF1F2937),
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                ),
+              ],
+            ),
+            SizedBox(height: 20.h,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(width: 20.w,),
+                Image.asset("assets/images/logo-facebook@2x.png",height: 20.h,width: 20.w,),
+                SizedBox(width: 35.w,),
+
+                CustomText(text:
+                'Continue with Facebook',
+                  textAlign: TextAlign.center,
+                  color: Color(0xFF1F2937),
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                ),
+              ],
+            ),
+            SizedBox(height: 50.h,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 103,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF4B5563),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 1,
+                        strokeAlign: BorderSide.strokeAlignCenter,
+                        color: Color(0xFF4B5563).withOpacity(0.1),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 9.w,),
+                CustomText(text: 'Or better yet...',
+                  textAlign: TextAlign.center,
+                  color: Color(0xFF4B5563),
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                ),
+                SizedBox(width: 9.w,),
+
+                Container(
+                  width: 103,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF4B5563),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 1,
+                        strokeAlign: BorderSide.strokeAlignCenter,
+                        color: Color(0xFF4B5563).withOpacity(0.1),
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+            SizedBox(height: 30.h,),
+            CustomTextFormFiled(hintText: 'Email or Mobile number',
+              controller: emailController,),
+            SizedBox(height: 10.h,),
+            CustomTextFormFiledPassword(hintText: 'Password',
+              controller: passwordController,),
+            SizedBox(height: 10.h,),
+
+            CustomTextFormFiledPassword(hintText: 'Confirm Password',
+              controller: confirmpasswordController,),
+            SizedBox(height: 10.h,),
+            SizedBox(height: 20.h,),
+            RoundButton(title: "Sign Up", onpress: (){},
+            ),
+            SizedBox(height: 5.h,),
+            Row(
+
+              children: [
+                Checkbox(
+                    activeColor: AppColors().blueMain,
+                    value: isChecked,
+                    shape: RoundedRectangleBorder(
+                    ),
+                    onChanged: (value){
+                      setState(() {
+                        isChecked = !isChecked;
+                        print(isChecked);
+                      });
+                    }),
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'By creating an account, you accept our\n',
+                        style: TextStyle(
+                          color: Color(0xFF4B5563),
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 0.13,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Terms and conditions',
+                        style: TextStyle(
+                          color: Color(0xFF4AC3B4),
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' you read\nour ',
+                        style: TextStyle(
+                          color: Color(0xFF4B5563),
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 0.13,
+
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Privacy Policy.',
+                        style: TextStyle(
+                          color: Color(0xFF4AC3B4),
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 0.13,
+
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
